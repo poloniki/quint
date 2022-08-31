6c1bd64652341ae4c602f232222063636ae0965a
Nikita Polovinkin <poloniki@ya.ru>
2022-08-31 16:26:35 +0200

    Creating a working flow of Audio to Text with FastAPI

--- Makefile
+++ Makefile
@@ -53,3 +53,6 @@ pypi_test:
 
 pypi:
 	@twine upload dist/* -u $(PYPI_USERNAME)
+
+run_api:
+	uvicorn fast:app --port 8082 --reload
--- fast.py
+++ fast.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from pydub import AudioSegment
-
+from quint.transcribtion import google_api as tga
 app = FastAPI()
 
 app.add_middleware(
@@ -19,10 +19,28 @@ def root():
 
 @app.post("/transcript")
 def upload(file: UploadFile = File(...)):
+
+
     try:
         contents = file.file.read()
         with open(file.filename, 'wb') as f:
+            # Save audio file locally
             f.write(contents)
+            # Get audio file namec
+            audio_file_name= file.filename
+            audio_file_name = audio_file_name.split('.')[0] + '.wav'
+            breakpoint()
+            # Get audio file transcribtion
+            transcript = tga.google_transcribe(audio_file_name)
+            # Create name for transcript
+            transcript_filename = audio_file_name.split('.')[0] + '.txt'
+            # Save transript file locally
+            tga.write_transcripts(transcript_filename,transcript)
+
+            # Return transcript to the api query
+            return transcript
+
+
     except Exception:
         return {"message": "There was an error uploading the file"}
     finally:
