8df81a4dbe4535c0e6fdde18c3a857a0a053b133
Nikita Polovinkin <poloniki@ya.ru>
2022-09-01 17:56:46 +0200

    Added returning colored transcript with best sentences to FASTAPI working for production

--- fast.py
+++ fast.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from pydub import AudioSegment
 from quint.transcribtion import google_api as tga
+from quint.transcribtion import highlights
 from quint.chunk.get_topics import get_topics
 import os
 output_filepath = os.getenv('OUTPUP_PATH')
@@ -23,35 +24,49 @@ def root():
 
 @app.post("/transcript")
 def upload(file: UploadFile = File(...)):
+    audio_file_name= file.filename
+    if audio_file_name not in os.listdir("."):
+        print('We got a new file')
 
+        try:
+            contents = file.file.read()
+            with open(file.filename, 'wb') as f:
+                # Get audio file namec
 
-    try:
-        contents = file.file.read()
-        with open(file.filename, 'wb') as f:
-            # Get audio file namec
-            audio_file_name= file.filename
-            audio_file_name = audio_file_name.split('.')[0] + '.wav'
-            if audio_file_name not in os.listdir("."):
-            # Save audio file locally
+                audio_file_name = audio_file_name.split('.')[0] + '.wav'
+                # if audio_file_name not in os.listdir("."):
+                # Save audio file locally
                 f.write(contents)
                 # # Get audio file transcribtion
                 transcript = tga.google_transcribe(audio_file_name)
+                # Get colored highlights
+                transcript = highlights.get_colored_transcript(transcript)
                 # Create name for transcript
                 transcript_filename = audio_file_name.split('.')[0] + '.txt'
                 # Save transript file locally
                 tga.write_transcripts(transcript_filename,transcript)
-            else:
-                return 'We already have this audio!'
 
-            # Return transcript to the api query
-            return  transcript
+                # else:
+                #     return 'We already have this audio!'
+
+                # Return transcript to the api query
+                return  transcript
 
 
-    except Exception as error:
-        return {"message": error}
+        except Exception as error:
+            return {"message": error}
 
-    finally:
-        file.file.close()
+        finally:
+            file.file.close()
+    else:
+        transcript_filename = audio_file_name.split('.')[0] + '.txt'
+        path_of_text = output_filepath + transcript_filename
+        transcript = highlights.get_colored_transcript(path_of_text)
+        transcript_filename = audio_file_name.split('.')[0] + '_colored'+'.txt'
+        tga.write_transcripts(transcript_filename,transcript)
+        return transcript
+
+
 
 # Get topics of an audio
 @app.post("/topics")
---
+++ quint/transcribtion/highlights.py
@@ -0,0 +1,65 @@
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy import sparse
+import numpy as np
+import pandas as pd
+from termcolor import colored
+import os
+
+SYMBOLS_STEP= os.getenv('SYMBOLS_STEP')
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+def create_embedding(path):
+    with open(path) as f:
+        doc = f.readlines()
+    doc = doc[0].replace("?", ". ")
+    sentences = doc.split('. ')
+    embeddings = model.encode(sentences)
+    print('Embeddings for highlights created')
+    return sentences, embeddings
+
+
+def create_df(sentences, embeddings):
+    df = pd.DataFrame()
+    df['sentence'] = sentences[:-1]
+    df['len'] = df['sentence'].apply(lambda x: len(x))
+    df['cum'] = df['len'].cumsum()
+    return df
+
+def get_best_sentences(df,embeddings):
+
+
+    best_sentences = []
+    steps = range(0, df.cum.max() + 8000, 8000)
+    for each in range(len(steps)-1):
+        temp_df = df.loc[(df.cum > steps[each])&(df.cum < steps[each+1])]
+        indexes = temp_df.index
+        A =  embeddings[indexes[0]:indexes[-1]+1]
+        A_sparse = sparse.csr_matrix(A)
+        similarities = cosine_similarity(A_sparse)
+        average_sim = list(np.round(similarities.mean(axis=1),2))
+
+        max_score = np.max(average_sim)
+        best_sentence = [i for i, x in enumerate(average_sim) if x > max_score-0.02]
+        for every in best_sentence:
+            best_sentences.append(temp_df.iloc[every].sentence)
+
+    best_sentences = [each for each in best_sentences if len(each)> 50]
+    df['highlight'] = df['sentence'].apply(lambda x: x in best_sentences)
+    print('Best sentences created.')
+
+    return best_sentences, df
+
+
+def get_colored_transcript(text):
+    sentences, embeddings = create_embedding(text)
+    df = create_df(sentences, embeddings)
+    best_sentences, df = get_best_sentences(df,embeddings)
+    text = ''
+    for num, each in enumerate(df['sentence']):
+        if df['highlight'].iloc[num] == True:
+            text+=colored(f'{each}. ','white','on_red')
+        else:
+            text+=f'{each}. '
+    print('Colored text ready.')
+    return text
