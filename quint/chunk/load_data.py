3606c7db186e3ad3cfb17cb21affbc1e63af6114
Ventus <29584664+V3ntus@users.noreply.github.com>
2023-01-04 19:02:18 -0600

    ref: remove question mark replace, add regex split

--- quint/chunk/load_data.py
+++ quint/chunk/load_data.py
@@ -12,13 +12,11 @@ def get_text_data(path_to_txt):
     with open(path_to_txt) as f:
         doc = f.readlines()
     if type(doc) is str:
-        doc = doc.replace("?", ".")
-        sentences = doc.split('.')
+        sentences = re.split(r'\.\s|\?\s|!\s', doc)
         print('Loaded Text String')
         return sentences
     elif type(doc) is list:
-        doc = doc[0].replace("?", ".")
-        sentences = doc.split('.')
+        sentences = re.split(r'\.\s|\?\s|!\s', doc[0])
 
         print('Data is loaded as a list')
         return sentences
