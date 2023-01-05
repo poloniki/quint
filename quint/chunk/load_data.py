47b78e516bf689927139a3303d6e9ff741de1a6e
V3ntus <29584664+V3ntus@users.noreply.github.com>
2023-01-05 15:47:33 -0600

    ref: fix regex to account for abbreviations when splitting

--- quint/chunk/load_data.py
+++ quint/chunk/load_data.py
@@ -12,11 +12,11 @@ def get_text_data(path_to_txt):
     with open(path_to_txt) as f:
         doc = f.readlines()
     if type(doc) is str:
-        sentences = re.findall('.*?[.!\?]', doc)
+        sentences = re.findall('.*?[.!\?]\s', doc)
         print('Loaded Text String')
         return sentences
     elif type(doc) is list:
-        sentences = re.findall('.*?[.!\?]', doc)
+        sentences = re.findall('.*?[.!\?]\s', doc)
 
         print('Data is loaded as a list')
         return sentences
--- quint/transcribtion/highlights.py
+++ quint/transcribtion/highlights.py
@@ -28,7 +28,7 @@ def create_embedding(path, version=1):
     # Replace question marks with period and split into sentences
     # doc = doc.replace('?', '. ')
     # sentences = doc.split('. ')
-    sentences = re.findall('.*?[.!\?]', doc)
+    sentences = re.findall('.*?[.!\?]\s', doc)
     # Encode the sentences using the model
     embeddings = model.encode(sentences)
     print('Embeddings for highlights created')
