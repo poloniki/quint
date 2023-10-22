492deee482890e561b61e5c7d222efd267d471da
Nikita Polovinkin <poloniki@ya.ru>
2023-10-22 17:40:04 +0200

    ❗️Cleaning frontend for future improvements

--- quint/frontend/chunk_api.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def chunk(text):
-    url = "https://imagequint-cro4ll255q-ew.a.run.app/chunk"
-    params = {'text':text}
-    response = requests.post(url, json=params)
-    chunks = response.json()['for_summary']
-    return chunks
